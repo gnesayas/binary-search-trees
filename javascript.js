27120038929d5c738b320baee1cfdf4001a94682
Gabriel <63373336+gnesayas@users.noreply.github.com>
2023-10-12 17:20:02 -0500

    Implement insert and remove functions for the binary search tree

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@
   const createTree = (arr) => {
     const validArray = removeDupes(mergeSort(arr));
 
-    const root = buildTree(validArray, 0, validArray.length - 1);
+    let root = buildTree(validArray, 0, validArray.length - 1);
 
     function mergeSort(array) {
       if (array.length < 2) {
@@ -64,7 +64,110 @@
       return node;
     }
 
-    return { root };
+    function insert(value) {
+      if (this.root === null) {
+        this.root = createNode(value);
+        return;
+      }
+      let node = this.root;
+      while (true) {
+        if (node.data === value) {
+          return;
+        } else if (node.data > value) {
+          if (node.left !== null) {
+            node = node.left;
+          } else {
+            node.left = createNode(value);
+            return;
+          }
+        } else if (node.data < value) {
+          if (node.right !== null) {
+            node = node.right;
+          } else {
+            node.right = createNode(value);
+            return;
+          }
+        }
+      }
+    }
+
+    function remove(value) {
+      if (this.root === null) {
+        return;
+      }
+      let node = this.root;
+      let isLeaf = !node.left && !node.right;
+      if (isLeaf) {
+        if (node.data === value) {
+          this.root = null;
+        }
+        return;
+      }
+      let parent = null;
+      let isLeftChild = false;
+      while (node) {
+        if (node.data === value) {
+          if (node.left && node.right) {
+            const successor = getSuccessor(node);
+            successor.left = node.left;
+            if (node.right !== successor) {
+              successor.right = node.right;
+            }
+            if (parent) {
+              if (isLeftChild) {
+                parent.left = successor;
+              } else {
+                parent.right = successor;
+              }
+            } else {
+              this.root = successor;
+            }
+          } else if (node.left) {
+            if (isLeftChild) {
+              parent.left = node.left;
+            } else {
+              parent.right = node.left;
+            }
+          } else if (node.right) {
+            if (isLeftChild) {
+              parent.left = node.right;
+            } else {
+              parent.right = node.right;
+            }
+          } else {
+            if (isLeftChild) {
+              parent.left = null;
+            } else {
+              parent.right = null;
+            }
+          }
+          return;
+        } else if (node.data > value) {
+          parent = node;
+          node = node.left;
+          isLeftChild = true;
+        } else {
+          parent = node;
+          node = node.right;
+          isLeftChild = false;
+        }
+      }
+    }
+
+    function getSuccessor(node) {
+      let currentNode = node.right;
+      let parent = null;
+      while (currentNode.left !== null) {
+        parent = currentNode;
+        currentNode = currentNode.left;
+      }
+      if (parent) {
+        parent.left = currentNode.right;
+      }
+      return currentNode;
+    }
+
+    return { root, insert, remove };
   };
 
   const prettyPrint = (node, prefix = "", isLeft = true) => {
@@ -82,5 +185,18 @@
 
   const nums = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
   const tree = createTree(nums);
+  //   prettyPrint(tree.root);
+
+  tree.insert(43);
+  //   prettyPrint(tree.root);
+
+  const empty = createTree([]);
+  //   prettyPrint(empty.root);
+  empty.insert(43);
+  //   prettyPrint(empty.root);
+
+  tree.insert(8.5);
+  prettyPrint(tree.root);
+  tree.remove(8);
   prettyPrint(tree.root);
 })();
